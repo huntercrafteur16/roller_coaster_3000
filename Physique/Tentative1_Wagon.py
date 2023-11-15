b29e25a59699930c688eb0370f3d5bc6a843cf04
Biot Arthur <arthur.biot@student-cs.fr>
2023-11-15 20:57:58 +0100

    Wagon fonctionel, pas encore en classe

--- Physique/Tentative1_Wagon.py
+++ Physique/Tentative1_Wagon.py
@@ -1,6 +1,4 @@
 import pymunk
-import os
-import platform
 import sys
 import pygame
 import pymunk.pygame_util
@@ -8,43 +6,36 @@ import random
 random.seed(1)
 import numpy as np
 from pymunk.vec2d import Vec2d
+from classes import *
+
+def add_wagon(space,m, L):
+
+    Box(space)
+    p = Vec2d(200, 150)
+    vs = [(-50, -30), (50, -30), (50, 30), (-50, 30)]
+    v0, v1, v2, v3 = vs
+    v4 = (0,70)
+    v5 = (0,30)
+    chassis = Poly(space,p, vs)
 
 
-def add_wagon(space,m,L):
-    ligne = pymunk.Body(mass=1, moment=1000, body_type= pymunk.Body.STATIC)
-    ligne_shape = pymunk.Segment(ligne, (0, 176), (200, 176), 8)
+    wheel1 = Circle(space,p+v2)
+    wheel2 = Circle(space,p+v3)
+    wheel3 = Circle(space, p+v4)
 
+    PivotJoint(space,chassis.body, wheel1.body, v2, (0, 0), False)
 
-    chassis = pymunk.Body(mass=m, moment=m)
-    chassis_shape = pymunk.Poly.create_box(chassis,(L,L))
-    chassis.position = (100,100)
-    roue1 = pymunk.Body(mass=1, moment=1)
-    roue2 = pymunk.Body(mass=1, moment=1)
-    roue1.position = (100-45, 100+45)
-    roue2.position = (100+45, 100+45)
-    roue3 = pymunk.Body(mass=1, moment=1)
-    roue4 = pymunk.Body(mass=1, moment=1)
-    roue1_shape = pymunk.Circle(roue1, radius = 25)
-    roue2_shape = pymunk.Circle(roue2, radius = 25)
+    PivotJoint(space,chassis.body, wheel2.body, v3, (0, 0), False)
 
-    roue1_shape.filter = pymunk.ShapeFilter(group=1)
-    roue2_shape.filter = pymunk.ShapeFilter(group=1)
-    chassis_shape.filter = pymunk.ShapeFilter(group=1)
-
-
-    roue3_shape = pymunk.Circle(roue3, radius = 15)
-    roue4_shape = pymunk.Circle(roue4, radius = 15)
-    joint02 = pymunk.PivotJoint(chassis, roue2, roue2.position)
-    joint01 = pymunk.PivotJoint(chassis, roue1,roue1.position)
+    DampedSpring(space, chassis.body, wheel3.body,v5,(0,0),1,700,1)
     
-    space.add(joint01, joint02, roue1, roue2, roue1_shape, roue2_shape, chassis, chassis_shape, ligne, ligne_shape)
 
 
 def animate(): 
     
     clock = pygame.time.Clock()
     space = pymunk.Space()
-    space.gravity = (0.0, 1*100)
+    space.gravity = (0.0, 900)
     wagon_body = add_wagon(space,10,100)
     screen = pygame.display.set_mode((600,600))
     draw_options = pymunk.pygame_util.DrawOptions(screen)
