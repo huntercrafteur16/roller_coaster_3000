0563cc674c7a4f3f0a5a856b62541ebb282647bc
AnouchkaDaniel <anouchka.daniel@student-cs.fr>
2023-11-15 16:20:35 +0100

    Update #Montagnes Russes.py

--- #Montagnes Russes.py
+++ #Montagnes Russes.py
@@ -7,25 +7,60 @@ import pymunk.pygame_util
 
 from geomdl import BSpline
 
-
+#Copier-coller aide d'internet
 rails = BSpline.Curve()# Create a 3-dimensional B-spline Curve
 rails.degree = 3# Set degree
 rails.ctrlpts = [[10, 5, 10], [10, 20, -30], [40, 10, 25], [-10, 5, 0]]# Set control points
 rails.knotvector = [0, 0, 0, 0, 1, 1, 1, 1]# Set knot vector
 rails.delta = 0.05# Set evaluation delta (controls the number of curve points)
 rails_points = rails.evalpts# Get curve points (the curve will be automatically evaluated)
-class GeneratingRails():
-    def add_rails(space):
+
+
+class Physique():
+    def __init__(self,screen):
+        self.draw_option = pymunk.pygame_util.DrawOptions(screen)
+        self.space = None
+        self.new()
+    
+    def new(self):
+        self.space = pymunk.Space()
+        self.space.gravity = 0, 200
+    #creation d'un objet static + la courbe est decrite par la liste des points bspline
+    #Question : comment definir bspline ?
+    def add_rails(self,bspline):
         rails = pymunk.Body(body_type = pymunk.Body.STATIC)
         rails.position = (300, 300)
         for i,p in enumerate(bspline[:-1]):
             rails = pymunk.Segment(self.space.static_body,p, bspline[i+1], 1)
             rails.elasticity = 1
             self.space.add(rails)
+    
+    #Ajouter l'objet à l'espace
+    def run(self):
+        self.space.debug_draw(self.draw_option)
+        self.space.step(0.01)
 
-    space.add(rails, rail_simple)
+    space.add(rails)
     return rail_simple
 
+
+class Spline():
+    def __init__(self,degree=2):
+        self.degree=degree
+        self.curve = BSpline.Curve()
+        self.curve.degree = degree
+        
+        self.points = []
+
+    def draw(self,points):
+        self.curve.ctrlpts = points
+        self.curve.knotvector = utilities.generate_knot_vector(self.curve.degree, len(self.curve.ctrlpts))
+        #self.curve.vis = VisMPL.VisCurve3D()
+        self.points=self.curve.evalpts
+        
+    def set_degree(self,d):
+        self.curve.degree = d
+        self.degree=d
 
 def main():
     pygame.init()
