0619013b997b24698755cfbc6a672ef211c5d1b2
Roy Fabien <fabien.roy@student-cs.fr>
2023-11-16 08:24:52 +0100

    Update Tentative1_Wagon.py

--- Tentative1_Wagon.py
+++ Tentative1_Wagon.py
@@ -1,3 +1,5 @@
+from pymunk.vec2d import Vec2d
+import numpy as np
 import os
 import platform
 import sys
@@ -7,26 +9,27 @@ import pymunk
 import pymunk.pygame_util
 import random
 random.seed(1)
-import numpy as np
-from pymunk.vec2d import Vec2d
 
 
-def add_wagon(space,m,L):
-    chassis = pymunk.Body(mass=m, moment=m)
-    
+def add_wagon(space, m, L):
+    ligne = pymunk.Body(body_type=pymunk.Body.STATIC)
+    ligne_shape = pymunk.Segment(ligne, (0, 300), (600, 300), 8)
+
+    wagon = pymunk.Body()
+    wagon_shape = pymunk.Poly(wagon, [(-5, -5), (5, -5), (5, 5), (-5, 5)])
+    space.add(ligne, ligne_shape, wagon, wagon_shape)
+    return
 
 
+def animate():
 
-def animate(): 
-    
     clock = pygame.time.Clock()
     space = pymunk.Space()
-    space.gravity = (0.0, 0*100)
-    wagon_body = add_wagon(space,10,100)
-    screen = pygame.display.set_mode((600,600))
+    space.gravity = (0.0, 900)
+    screen = pygame.display.set_mode((600, 600))
+    wagon_body = add_wagon(space, 10, 100)
     draw_options = pymunk.pygame_util.DrawOptions(screen)
 
-    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -34,11 +37,11 @@ def animate():
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 sys.exit(0)
 
-        
-        screen.fill((255,255,255))
+        screen.fill((255, 255, 255))
         space.debug_draw(draw_options)
         space.step(1/50.0)
         pygame.display.flip()
         clock.tick(50)
+
 
 animate()
