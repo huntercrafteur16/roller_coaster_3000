7da2a33edd95c44d0cb756386a701c0ccb6891e8
Clément Bonizec <clement.bonizec@student-cs.fr>
2023-11-17 15:46:43 +0100

    Changements interface et test_GUI

--- GUI/interface.py
+++ GUI/interface.py
@@ -1,7 +1,6 @@
 from tkinter import *
-
-from matplotlib.sankey import UP
-from GUI.graphiques import graphe_vitesse, graphe_accel
+# from GUI.graphiques import AnimatedGraph
+import time
 
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
@@ -13,22 +12,28 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 class Interface():
-    def __init__(self, dico_variables, dico_functions):
-        self.var = dico_variables
-        self.func = dico_functions
+    def __init__(self):
 
         # Fenêtre principale
         self.root = Tk()
         self.root.title("Bienvenue dans le Roller Coaster 3000, visiteur !")
 
-        # Deux frames : la barre d'outils et la simulation
+        # Trois frames : barre d'outils, simulation, barre des graphes
         toolbar = Frame(self.root, bg="lightgray")
         simu = Frame(self.root, borderwidth=5, bg="white")
+        graphbar = Frame(self.root, bg="lightgray")
         toolbar.pack(side=TOP, expand=False, fill=X)
-        simu.pack(side=BOTTOM, expand=True, fill=BOTH)
+        simu.pack(expand=True, fill=BOTH)
+        graphbar.pack(side=BOTTOM, fill=X, expand=False)
 
         # Variables
-        m, v, f = DoubleVar(value=1), DoubleVar(value=1), DoubleVar(value=0.1)
+        self.applied_m, self.applied_f, self.applied_c = 1, 10, 0.1
+        m, f, c = DoubleVar(value=1), DoubleVar(value=10), DoubleVar(value=0.1)
+
+        def apply_values():
+            self.applied_m = float(m.get())
+            self.applied_f = float(f.get())
+            self.applied_c = float(c.get())
 
         # Boutons Start/Reset
         buttons = Frame(toolbar, bg="lightgray", height=100, padx=5)
@@ -37,125 +42,110 @@ class Interface():
         def doNothing():
             pass
 
-        start_reset = Button(buttons, command=doNothing, width=10, height=2,
-                             text='Start/Reset', fg='#30b000', activebackground='#30b000')
-        play_pause = Button(buttons, command=doNothing, width=10, height=2,
-                            text='Play/Pause', fg='#0080ff', activebackground='#0080ff')
-        start_reset.grid(row=0, column=0, padx=3, pady=3)
-        play_pause.grid(row=0, column=1, padx=3, pady=3)
+        self.start_reset = Button(buttons, command=doNothing, width=10, height=2,
+                                  text='Start/Reset', fg='#30b000', activebackground='#30b000')
+        self.play_pause = Button(buttons, command=doNothing, width=10, height=2,
+                                 text='Play/Pause', fg='#0080ff', activebackground='#0080ff')
+        self.start_reset.grid(row=0, column=0, padx=3, pady=3)
+        self.play_pause.grid(row=0, column=1, padx=3, pady=3)
 
         # Paramètres
         param = Frame(toolbar, bg="lightgray", width=200, padx=5, pady=5)
         param.grid(row=0, column=1)
 
         apply_param = Button(param, height=2, text='Appliquer les paramètres',
-                             bg='lightgray', fg='red', activebackground='red', command=doNothing)
+                             bg='lightgray', fg='red', activebackground='red', command=apply_values)
         apply_param.grid(row=0, column=0, padx=5, pady=5)
 
         # masse
         param_m = Frame(param)
         label_m = Label(param_m, text='Masse du wagon (kg)',
-                        width=20, height=1)
+                        width=25, height=1)
         entry_m = Entry(param_m, textvariable=m, width=5)
         scale_m = Scale(param_m, from_=0, to=100, showvalue=False, variable=m,
                         tickinterval=25, orient=HORIZONTAL, width=10)
         # vitesse
-        param_v = Frame(param)
-        label_v = Label(param_v, text='Vitesse des treuils (m/s)',
-                        width=20, height=1)
-        entry_v = Entry(param_v, textvariable=v, width=5)
-        scale_v = Scale(param_v, from_=0, to=100, showvalue=False, variable=v,
+        param_F = Frame(param)
+        label_F = Label(param_F, text='Force des treuils (N)',
+                        width=25, height=1)
+        entry_F = Entry(param_F, textvariable=f, width=5)
+        scale_F = Scale(param_F, from_=0, to=100, showvalue=False, variable=f,
                         tickinterval=25, orient=HORIZONTAL, width=10)
         # coef de frottement
         param_f = Frame(param)
         label_f = Label(param_f, text='Coef. de frottement (kg/m)',
-                        width=20, height=1)
-        entry_f = Entry(param_f, textvariable=f, width=5)
-        scale_f = Scale(param_f, from_=0, to=10, showvalue=False, variable=f,
+                        width=25, height=1)
+        entry_f = Entry(param_f, textvariable=c, width=5)
+        scale_f = Scale(param_f, from_=0, to=10, showvalue=False, variable=c,
                         tickinterval=2, orient=HORIZONTAL, width=10)
 
+        # On affiche tout
         label_m.grid(row=0, column=1, columnspan=2)
         entry_m.grid(row=1, column=1, padx=3)
         scale_m.grid(row=1, column=2)
-        param_m.grid(row=0, column=1, padx=5, pady=5)
+        param_m.grid(row=0, column=1, padx=10, pady=5)
 
-        label_v.grid(row=0, column=1, columnspan=2)
-        entry_v.grid(row=1, column=1, padx=3)
-        scale_v.grid(row=1, column=2)
-        param_v.grid(row=0, column=2, padx=5, pady=5)
+        label_F.grid(row=0, column=1, columnspan=2)
+        entry_F.grid(row=1, column=1, padx=3)
+        scale_F.grid(row=1, column=2)
+        param_F.grid(row=0, column=2, padx=10, pady=5)
 
         label_f.grid(row=0, column=1, columnspan=2)
         entry_f.grid(row=1, column=1, padx=3)
         scale_f.grid(row=1, column=2)
-        param_f.grid(row=0, column=3, padx=5, pady=5)
+        param_f.grid(row=0, column=3, padx=10, pady=5)
 
         # A l'intérieur de simu : roller coaster et graphe
+        self.roller_coaster = Frame(simu, bg='white')
+        self.frame_graph = Frame(graphbar, bg='blue')
+        self.roller_coaster.grid(row=1, column=1, rowspan=3, columnspan=3)
 
-        roller_coaster = Frame(simu, bg='white')
-        frame_graph = Frame(simu)
-        roller_coaster.grid(row=1, column=1, rowspan=3, columnspan=3)
+        # A l'intérieur de graphbar, choix des graphes.
 
         # Boutons radio pour voir ou non un des graphes proposés
-        void = Frame(toolbar, width=15, bg="lightgray", padx=5)
-        graph_choice = Frame(toolbar, padx=5)
-        void.grid(row=0, column=2)
-        graph_choice.grid(row=0, column=3)
+        graph_choice = Frame(graphbar, height=80)
+        graph_choice.pack(side=LEFT, expand=False, padx=5, pady=5)
 
-        choice = IntVar(value=0)
+        self.choice = IntVar(value=0)
         label_graph_choice = Label(
-            graph_choice, text="Choix du graphe à afficher")
-        label_graph_choice.pack(side=TOP)
+            graph_choice, text="Choix du graphe à afficher", height=2)
+        label_graph_choice.grid(row=0, column=0, padx=5, pady=5)
 
-        Radiobuttons = Frame(graph_choice)
-        Radiobuttons.pack(side=BOTTOM)
+        Radiobuttons = Frame(graph_choice, borderwidth=5)
+        Radiobuttons.grid(row=1, column=0)
 
-        # Fonctions des boutons qui réalisent les graphes matplotlib
-        List_speeds, List_accels = self.var['List_speeds'], self.var['List_accels']
-        t_graph = DoubleVar(value=10)
-        # empty_graph = plt.figure()
-        # graph = FigureCanvasTkAgg(empty_graph, master=frame_graph)
+        Radiobutton(Radiobuttons, variable=self.choice, value='none',
+                    height=1).grid(row=1, column=1, sticky=E)
+        Label(Radiobuttons, text='Aucun graphe', width=15,
+              height=1).grid(row=1, column=0, sticky=E)
 
-        # def renderNothing():
-        #    for widget in frame_graph.winfo_children():
-        #        widget.destroy()
-        #    pass
+        Radiobutton(Radiobuttons, variable=self.choice, value='speeds',
+                    height=1).grid(row=2, column=1, sticky=E)
+        Label(Radiobuttons, text='Vitesse(t)',
+              width=15, height=1).grid(row=2, column=0, sticky=E)
 
-        # def renderSpeeds():
-        #    for widget in frame_graph.winfo_children():
-        #        widget.destroy()
-        #    graph.figure = graphe_vitesse(List_speeds, t_graph.get())
-        #    canvas = graph.get_tk_widget()
-        #    canvas.pack()
-        #    frame_graph.grid(row=0, column=3, sticky=E)
+        Radiobutton(Radiobuttons, variable=self.choice, value='accels',
+                    height=1).grid(row=3, column=1, sticky=E)
+        Label(Radiobuttons, text='Accélération(t)',
+              width=15, height=1).grid(row=3, column=0, sticky=E)
 
-        # def renderAccels():
-        #    for widget in frame_graph.winfo_children():
-        #        widget.destroy()
-        #    graph.figure = graphe_accel(List_accels, t_graph.get())
-        #    canvas = graph.get_tk_widget()
-        #    canvas.pack()
-        #    frame_graph.grid(row=0, column=3, sticky=E)
+        self.t_graph = DoubleVar(value=10)
+        Label(Radiobuttons, text='Durée de mesure (s): ', width=15,
+              height=1).grid(row=4, column=0, sticky=E)
+        Entry(Radiobuttons, textvariable=self.t_graph, width=4).grid(
+            row=4, column=1, sticky=E)
 
-        Radiobutton(Radiobuttons, variable=choice, value=1,
-                    height=1, command=doNothing).grid(row=1, column=0)
-        Label(Radiobuttons, text='aucun', width=4,
-              height=1).grid(row=1, column=1)
-        Label(Radiobuttons, text=' ', width=3, height=1).grid(row=1, column=2)
-        Radiobutton(Radiobuttons, variable=choice, value=2,
-                    height=1, command=doNothing).grid(row=1, column=3)
-        Label(Radiobuttons, text='vitesse(t)',
-              width=6, height=1).grid(row=1, column=4)
-        Label(Radiobuttons, text=' ', width=3, height=1).grid(row=1, column=5)
-        Radiobutton(Radiobuttons, variable=choice, value=3,
-                    height=1, command=doNothing).grid(row=1, column=6)
-        Label(Radiobuttons, text='accélération(t)',
-              width=10, height=1).grid(row=1, column=7)
-        Label(Radiobuttons, text=' ', width=3, height=1).grid(row=1, column=8)
+    # Donne la frame qui doit contenir un graphe et le choix de graphe associé
+    def get_graph_frame(self):
+        return (self.frame_graph, self.choice.get())
 
-        Label(Radiobuttons, text='Durée (s): ', width=7,
-              height=1).grid(row=1, column=9, sticky=E)
-        Entry(Radiobuttons, textvariable=t_graph, width=4).grid(
-            row=1, column=10, sticky=E, padx=5, pady=5)
+    # Donne la frame dans laquelle
+    def get_pymunk_frame(self):
+        return self.roller_coaster
 
-    def renderGUI(self):
-        self.root.mainloop()
+    # Rend les valeurs choisies
+    def get_param(self):
+        return {'mass': self.applied_m, 'force': self.applied_f, 'frict': self.applied_c}
+
+    def render_GUI(self):
+        self.root.update()
--- test_GUI.py
+++ test_GUI.py
@@ -1,12 +1,11 @@
 from GUI.interface import *
-from GUI.graphiques import *
+from GUI.graphiques import AnimatedGraph
+import time
 
-var = {}
-func = {}
-List_speeds = [1, 3, 5, 8, 14, 12, 5, 4, 6, 9, 9, 8, 5, 4, 2]
-List_accels = [1, 2, 1, 5, 7, 11, 17, 18, 18, 17, 14, 8, 5, 4, 2]
-var['List_speeds'] = List_speeds
-var['List_accels'] = List_accels
+GUI = Interface()
+GUI.render_GUI()
 
-GUI = Interface(var, func)
-GUI.renderGUI()
+while True:
+    print(GUI.get_param())
+    time.sleep(1/60)
+    GUI.render_GUI()
