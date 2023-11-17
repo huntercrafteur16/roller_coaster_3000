48c71909fe44e3064e8cac77bb50ced93f45d23e
Vallade Antoine <antoine.vallade@student-cs.fr>
2023-11-17 20:23:36 +0100

    animated_graphs can now be easily embedded in tkinter frames

--- GUI/graphiques.py
+++ GUI/graphiques.py
@@ -1,7 +1,8 @@
 from time import sleep
+from tkinter import Frame
 from pylab import *
 from matplotlib import pyplot as plt
-
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 # On veut afficher le graphe en temps réel, pendant l'animation du wagon.
 # Pour cela on modélise la courbe par sa liste des vitesses discrétisée par dt
 # et on bidouille
@@ -31,18 +32,25 @@ def graphe_vitesse(List_speeds, tf):
 
 
 class AnimatedGraph():
-    def __init__(self, tlim: tuple, datalim: tuple, title) -> None:
+    """
+    Classe qui permet de réaliser des graphes à animer sous tkinter avec des données envoyées en temps réel
+    """
+
+    def __init__(self, title) -> None:
         self.t = []
         self.data = []
         self.fig = plt.figure()
-        xlim(tlim)
-        ylim(datalim)
-        plt.title = title
+        self._ymax = 0
+        xlim(0, 0)
+        plt.title(title)
         ion()
         self.curve, = plt.plot(self.t, self.data)
 
     def drawNext(self, t, data) -> None:
+        xlim((0, t))
 
+        if self._ymax <= abs(data):
+            ylim((-abs(data), abs(data)))
         self.t.append(t)  # type: ignore
         self.data.append(data)  # type: ignore
         self.curve.set_xdata(self.t)
@@ -59,6 +67,11 @@ class AnimatedGraph():
     def fixDisplay(self):
         ioff()
         show()
+
+    def attach_to_frame(self, frame: Frame):
+        FigureCanvasTkAgg(
+            self.fig, frame).get_tk_widget().pack()
+
 
 # graphe_vitesse([0.02*x**4-x**2+3*x for x in linspace(0, 5, 50)], 5)
 
--- main.py
+++ main.py
@@ -6,20 +6,21 @@ from GUI.graphiques import AnimatedGraph
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from Physique.wagon import Wagon
 
-
-interface = Interface()
+interface = Interface()  # génération de l'objet génrant l'interface principal
 
 manager = physicManager(interface.get_pymunk_frame().winfo_width(), interface.get_pymunk_frame().winfo_height(), interface.simu,
-                        interface.get_pymunk_frame())
+                        interface.get_pymunk_frame())  # génération du physicManager
 
+# graphe de représentation de vitesse
+vitesse_graph = AnimatedGraph("viteeeeeeeeeeeeeeesse")
 
-vitesse_graph = AnimatedGraph((0, 1000), (-1e4, 1e4), "viteeeeeeeeeeeeeeesse")
-graph = FigureCanvasTkAgg(
-    vitesse_graph.fig, master=interface.get_graph_frame()[0])
+# on le connecte à la frame tkinter voulue
+vitesse_graph.attach_to_frame(interface.get_graph_frame()[0])
+
+# On n'oublie pas de pack le conteneur et #TODO il faudra le faire directement dans interface
 interface.get_graph_frame()[0].pack()
-canva = graph.get_tk_widget()
-canva.pack()
-cont = True
+
+cont = True  # continuer l'exécution du programme
 i = 0
 while cont:
     vitesse_graph.drawNext(
