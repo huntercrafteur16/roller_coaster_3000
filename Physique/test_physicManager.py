b1f50379a95844bd72c1245e80aff9d18dcb8cb7
Biot Arthur <arthur.biot@student-cs.fr>
2023-11-23 11:54:59 +0100

    fichier tests

--- Physique/test_physicManager.py
+++
@@ -1,9 +0,0 @@
-"""
-Module de test du fichier physicmanager
-"""
-from physicManager import physicManager
-manager = physicManager(1000, 600)
-
-cont = True
-while cont is True:
-    cont = manager.process()
